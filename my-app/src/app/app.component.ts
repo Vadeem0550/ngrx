8b85a08173233c26ef715b38430a3d512da28d73
Vadim <65122882+Vadeem0550@users.noreply.github.com>
2022-05-02 22:20:14 +0300

    add add effect on update time

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {Store} from "@ngrx/store";
-import {clear, countSelector, decrease, increase} from "./reducers/counter";
+import {clear, countSelector, decrease, increase, updatedAdSelector} from "./reducers/counter";
 import {map} from "rxjs";
 
 @Component({
@@ -9,26 +9,23 @@ import {map} from "rxjs";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  updateAt?: number;
   count$ = this._store.select(countSelector)
   cannotDecrease$ = this.count$.pipe(map(count => count <= 0));
+  updatedAt$ = this._store.select(updatedAdSelector);
 
   constructor(private _store: Store) {
   }
 
   increase(): void {
-    this.updateAt = Date.now()
     this._store.dispatch(increase())
   }
 
   decrease(): void {
-    this.updateAt = Date.now()
     this._store.dispatch(decrease())
 
   }
 
   clear(): void {
-    this.updateAt = Date.now()
     this._store.dispatch(clear())
   }
 }
