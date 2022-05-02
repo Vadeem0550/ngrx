885952e069a8f82885dd5dd1212c03876e149a6a
Vadim <65122882+Vadeem0550@users.noreply.github.com>
2022-05-02 18:50:02 +0300

    NGRX-1 add counter code

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  counter = 0;
+  updateAt?: number;
+
+  get cannotDecrease(): boolean {
+    return this.counter <= 0;
+  }
+
+  increase(): void {
+    this.updateAt = Date.now()
+    this.counter++;
+  }
+
+  decrease(): void {
+    this.updateAt = Date.now()
+    this.counter--;
+  }
+
+  clear(): void {
+    this.updateAt = Date.now()
+    this.counter = 0;
+  }
+}
